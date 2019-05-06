c49639d07f2432baa93a4fbe84ee379f58012d04
Daniel Alejandro Oliveros Duran <daoliverosd@unal.edu.co>
2019-05-05 21:26:07 -0500

    Update script.sh
    Datos a mayuscula para las letras minisculas.

--- script.sh
+++ script.sh
@@ -1,2 +1,8 @@
 # Escriba su código aquí
 sed 's#\([0-9][0-9]\)/\([0-9][0-9]\)/\([0-9][0-9]\)#20\3-\2-\1#' data.csv > dataYearFormat.csv 
+sed 's#[a-z]#\U&#g' dataYearFormat.csv > dataToUpperCase.csv 
+
+sed 's#\\N# #g' dataYearFormat.csv  > dataNCorrection.csv
+sed 's#;;#; ;#g' dataNCorrection.csv  > dataNCorrectionNoSlash.csv
+sed 's#;;#;\\N;#g' dataNCorrectionNoSlash.csv  > dataReplaceSlashN.csv
+sed 's#\(.*\);#\1#g' dataNoNull.csv  > dataNoNullCorrection.csv
