004c67f0f4ded31ff8e41bdb94d33aead0990ec1
Daniel Alejandro Oliveros Duran <daoliverosd@unal.edu.co>
2019-05-05 21:29:51 -0500

    Update script.sh
    Remplazo de , por  .

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 # Escriba su código aquí
 sed 's#\([0-9][0-9]\)/\([0-9][0-9]\)/\([0-9][0-9]\)#20\3-\2-\1#' data.csv > dataYearFormat.csv 
 sed 's#[a-z]#\U&#g' dataYearFormat.csv > dataToUpperCase.csv 
+sed 's#,#.#g' dataToUpperCase.csv > dataWithOutComma.csv 
 
 sed 's#\\N# #g' dataYearFormat.csv  > dataNCorrection.csv
 sed 's#;;#; ;#g' dataNCorrection.csv  > dataNCorrectionNoSlash.csv
